1ad5ee5ace2e701e0d3a2ad9675a118ce6c8793d
Ethan Sagin <ethansagin@gmail.com>
2019-06-06 15:00:41 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -10,10 +10,12 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
-  post '/puppy'
+  post '/puppy' do
     @name = params[:name]
     @breed = params[:breed]
     @age = params[:months_old]
+    
+    erb :display_puppy
   end
 
 end
